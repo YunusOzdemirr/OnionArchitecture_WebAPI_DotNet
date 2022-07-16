bab1fe89f5644f5b205d74599a1e5faf82911355
yunusozdemirr <yunusozdemir468@gmail.com>
2022-07-16 15:58:00 +0300

    Domain layer added

---
+++ src/Core/ProductApp.Domain/Common/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime;
+
+namespace ProductApp.Domain.Common
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
+
---
+++ src/Core/ProductApp.Domain/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ProductApp.Domain.Common;
+
+namespace ProductApp.Domain.Entities
+{
+    public class Product:BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public int Quantity { get; set; }
+    }
+}
+
